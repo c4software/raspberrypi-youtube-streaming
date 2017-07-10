80df9cca7daa82912ee5ab62075fd77239edeb86
Brosseau Valentin <c4software@gmail.com>
2017-07-10 13:32:26 +0200

    Update entry.sh

--- streaming/entry.sh
+++ streaming/entry.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+SECRET=$1
 W=${2:-"1920"}
 H=${3:-"1080"}
 
 echo Live-stream secret: $1
 
-raspivid -o - -t 0 -w $W -h $H -fps 40 -b 8000000 -g 40 | ffmpeg -re -ar 44100 -ac 2 -acodec pcm_s16le -f s16le -ac 2 -i /dev/zero -f h264 -i pipe:0 -c:v copy -c:a aac -ab 128k -g 40 -strict experimental -f flv -r 30 rtmp://a.rtmp.youtube.com/live2/$1
+raspivid -o - -t 0 -w $W -h $H -fps 24 -g 24 | ffmpeg -re -f lavfi -i anullsrc -f h264 -i pipe:0 -c:v copy -ar 44100 -ac 1 -acodec pcm_s16le -f s16le -strict experimental -f flv rtmp://a.rtmp.youtube.com/live2/$1
